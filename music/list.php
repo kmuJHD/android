b542def66433e08518344bf4877bc56365c4829b
kmuJHD <temple@kmu.ac.kr>
2017-06-14 11:12:58 +0900

    Create list.php

---
+++ music/list.php
@@ -0,0 +1,15 @@
+<?
+	if($_GET["mode"] == "list")
+	{
+		$connect= mysqli_connect('mysql','temple','wer5113@@8', 'android');
+		$connect->set_charset("utf8");
+		$sql = "select * from Music";
+		$result = mysqli_query($connect, $sql);
+		$num = mysqli_num_rows($result);
+    
+    while ($field = $result->fetch_assoc()) {
+      echo $field["name"]."\n";
+    }
+
+	}
+?>
